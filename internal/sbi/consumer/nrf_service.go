e5dd0ae2ddfda052b16023f481dab108483dcec9
HanHongChen <chh.cs12@nycu.edu.tw>
2024-04-22 14:32:13 +0000

    refactor: consumer-finish

---
+++ internal/sbi/consumer/nrf_service.go
@@ -0,0 +1,300 @@
+package consumer
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+	"time"
+	"sync"
+
+	"github.com/antihax/optional"
+	"github.com/pkg/errors"
+
+	"github.com/free5gc/openapi"
+	"github.com/free5gc/openapi/Nnrf_NFDiscovery"
+	"github.com/free5gc/openapi/Nnrf_NFManagement"
+	"github.com/free5gc/openapi/models"
+	pcf_context "github.com/free5gc/pcf/internal/context"
+	"github.com/free5gc/pcf/internal/logger"
+	"github.com/free5gc/pcf/internal/util"
+)
+
+type nnrfService struct{
+	consumer *Consumer
+
+	nfMngmntMu sync.RWMutex
+	nfDiscMu   sync.RWMutex 
+
+	nfMngmntClients map[string]*Nnrf_NFManagement.APIClient
+	nfDiscClients   map[string]*Nnrf_NFDiscovery.APIClient
+
+}
+
+func (s *nnrfService) getNFManagementClient(uri string) *Nnrf_NFManagement.APIClient {
+	if uri == "" {
+		return nil
+	}
+	s.nfMngmntMu.RLock()
+	client, ok := s.nfMngmntClients[uri]
+	if ok {
+		defer s.nfMngmntMu.RUnlock()
+		return client
+	}
+
+	configuration := Nnrf_NFManagement.NewConfiguration()
+	configuration.SetBasePath(uri)
+	client = Nnrf_NFManagement.NewAPIClient(configuration)
+
+	s.nfMngmntMu.RUnlock()
+	s.nfMngmntMu.Lock()
+	defer s.nfMngmntMu.Unlock()
+	s.nfMngmntClients[uri] = client
+	return client
+}
+
+func (s *nnrfService) getNFDiscClient(uri string) *Nnrf_NFDiscovery.APIClient{
+	if uri == "" {
+		return nil
+	}
+	s.nfDiscMu.RLock()
+	client, ok := s.nfDiscClients[uri]
+	if ok {
+		defer s.nfDiscMu.RUnlock()
+		return client
+	}
+
+	configuration := Nnrf_NFDiscovery.NewConfiguration()
+	configuration.SetBasePath(uri)
+	client = Nnrf_NFDiscovery.NewAPIClient(configuration)
+
+	s.nfDiscMu.RUnlock()
+	s.nfDiscMu.Lock()
+	defer s.nfDiscMu.Unlock()
+	s.nfDiscClients[uri] = client
+	return client
+
+}
+func (s *nnrfService) SendSearchNFInstances(
+	nrfUri string, targetNfType, requestNfType models.NfType, param Nnrf_NFDiscovery.SearchNFInstancesParamOpts) (
+	*models.SearchResult, error,
+) {
+	// Set client and set url
+	client := s.getNFDiscClient(nrfUri)
+
+	ctx, _, err := pcf_context.GetSelf().GetTokenCtx(models.ServiceName_NNRF_DISC, models.NfType_NRF)
+	if err != nil {
+		return nil, err
+	}
+
+	result, res, err := client.NFInstancesStoreApi.SearchNFInstances(ctx, targetNfType, requestNfType, &param)
+	if err != nil {
+		logger.ConsumerLog.Errorf("SearchNFInstances failed: %+v", err)
+	}
+	defer func() {
+		if resCloseErr := res.Body.Close(); resCloseErr != nil {
+			logger.ConsumerLog.Errorf("NFInstancesStoreApi response body cannot close: %+v", resCloseErr)
+		}
+	}()
+	if res != nil && res.StatusCode == http.StatusTemporaryRedirect {
+		return nil, fmt.Errorf("Temporary Redirect For Non NRF Consumer")
+	}
+
+	return &result, nil
+}
+
+func (s *nnrfService) SendNFInstancesUDR(nrfUri, id string) string {
+	targetNfType := models.NfType_UDR
+	requestNfType := models.NfType_PCF
+	localVarOptionals := Nnrf_NFDiscovery.SearchNFInstancesParamOpts{
+		// 	DataSet: optional.NewInterface(models.DataSetId_SUBSCRIPTION),
+	}
+	// switch types {
+	// case NFDiscoveryToUDRParamSupi:
+	// 	localVarOptionals.Supi = optional.NewString(id)
+	// case NFDiscoveryToUDRParamExtGroupId:
+	// 	localVarOptionals.ExternalGroupIdentity = optional.NewString(id)
+	// case NFDiscoveryToUDRParamGpsi:
+	// 	localVarOptionals.Gpsi = optional.NewString(id)
+	// }
+
+	result, err := s.SendSearchNFInstances(nrfUri, targetNfType, requestNfType, localVarOptionals)
+	if err != nil {
+		logger.ConsumerLog.Error(err.Error())
+		return ""
+	}
+	for _, profile := range result.NfInstances {
+		if uri := util.SearchNFServiceUri(profile, models.ServiceName_NUDR_DR, models.NfServiceStatus_REGISTERED); uri != "" {
+			return uri
+		}
+	}
+	return ""
+}
+
+func (s *nnrfService) SendNFInstancesBSF(nrfUri string) string {
+	targetNfType := models.NfType_BSF
+	requestNfType := models.NfType_PCF
+	localVarOptionals := Nnrf_NFDiscovery.SearchNFInstancesParamOpts{}
+
+	result, err := s.SendSearchNFInstances(nrfUri, targetNfType, requestNfType, localVarOptionals)
+	if err != nil {
+		logger.ConsumerLog.Error(err.Error())
+		return ""
+	}
+	for _, profile := range result.NfInstances {
+		if uri := util.SearchNFServiceUri(profile, models.ServiceName_NBSF_MANAGEMENT,
+			models.NfServiceStatus_REGISTERED); uri != "" {
+			return uri
+		}
+	}
+	return ""
+}
+
+func (s *nnrfService) SendNFInstancesAMF(nrfUri string, guami models.Guami, serviceName models.ServiceName) string {
+	targetNfType := models.NfType_AMF
+	requestNfType := models.NfType_PCF
+
+	localVarOptionals := Nnrf_NFDiscovery.SearchNFInstancesParamOpts{
+		Guami: optional.NewInterface(util.MarshToJsonString(guami)),
+	}
+	// switch types {
+	// case NFDiscoveryToUDRParamSupi:
+	// 	localVarOptionals.Supi = optional.NewString(id)
+	// case NFDiscoveryToUDRParamExtGroupId:
+	// 	localVarOptionals.ExternalGroupIdentity = optional.NewString(id)
+	// case NFDiscoveryToUDRParamGpsi:
+	// 	localVarOptionals.Gpsi = optional.NewString(id)
+	// }
+
+	result, err := s.SendSearchNFInstances(nrfUri, targetNfType, requestNfType, localVarOptionals)
+	if err != nil {
+		logger.ConsumerLog.Error(err.Error())
+		return ""
+	}
+	for _, profile := range result.NfInstances {
+		return util.SearchNFServiceUri(profile, serviceName, models.NfServiceStatus_REGISTERED)
+	}
+	return ""
+}
+//management
+func (s *nnrfService) BuildNFInstance(context *pcf_context.PCFContext) (profile models.NfProfile, err error) {
+	profile.NfInstanceId = context.NfId
+	profile.NfType = models.NfType_PCF
+	profile.NfStatus = models.NfStatus_REGISTERED
+	profile.Ipv4Addresses = append(profile.Ipv4Addresses, context.RegisterIPv4)
+	service := []models.NfService{}
+	for _, nfService := range context.NfService {
+		service = append(service, nfService)
+	}
+	profile.NfServices = &service
+	profile.PcfInfo = &models.PcfInfo{
+		DnnList: []string{
+			"free5gc",
+			"internet",
+		},
+		// SupiRanges: &[]models.SupiRange{
+		// 	{
+		// 		//from TS 29.510 6.1.6.2.9 example2
+		//		//no need to set supirange in this moment 2019/10/4
+		// 		Start:   "123456789040000",
+		// 		End:     "123456789059999",
+		// 		Pattern: "^imsi-12345678904[0-9]{4}$",
+		// 	},
+		// },
+	}
+	if context.Locality != "" {
+		profile.Locality = context.Locality
+	}
+	return
+}
+
+func (s *nnrfService) SendRegisterNFInstance(nrfUri, nfInstanceId string, profile models.NfProfile) (
+	resouceNrfUri string, retrieveNfInstanceID string, err error,
+) {
+	// Set client and set url
+	client := s.getNFManagementClient(nrfUri)
+
+	ctx, _, err := pcf_context.GetSelf().GetTokenCtx(models.ServiceName_NNRF_NFM, models.NfType_NRF)
+	if err != nil {
+		return "", "",
+			errors.Errorf("SendRegisterNFInstance error: %+v", err)
+	}
+
+	var res *http.Response
+	var nf models.NfProfile
+	for {
+		nf, res, err = client.NFInstanceIDDocumentApi.RegisterNFInstance(ctx, nfInstanceId, profile)
+		if err != nil || res == nil {
+			// TODO : add log
+			fmt.Println(fmt.Errorf("PCF register to NRF Error[%v]", err.Error()))
+			time.Sleep(2 * time.Second)
+			continue
+		}
+		defer func() {
+			if resCloseErr := res.Body.Close(); resCloseErr != nil {
+				logger.ConsumerLog.Errorf("RegisterNFInstance response body cannot close: %+v", resCloseErr)
+			}
+		}()
+		status := res.StatusCode
+		if status == http.StatusOK {
+			// NFUpdate
+			break
+		} else if status == http.StatusCreated {
+			// NFRegister
+			resourceUri := res.Header.Get("Location")
+			resouceNrfUri = resourceUri[:strings.Index(resourceUri, "/nnrf-nfm/")]
+			retrieveNfInstanceID = resourceUri[strings.LastIndex(resourceUri, "/")+1:]
+
+			oauth2 := false
+			if nf.CustomInfo != nil {
+				v, ok := nf.CustomInfo["oauth2"].(bool)
+				if ok {
+					oauth2 = v
+					logger.MainLog.Infoln("OAuth2 setting receive from NRF:", oauth2)
+				}
+			}
+			pcf_context.GetSelf().OAuth2Required = oauth2
+
+			if oauth2 && pcf_context.GetSelf().NrfCertPem == "" {
+				logger.CfgLog.Error("OAuth2 enable but no nrfCertPem provided in config.")
+			}
+			break
+		} else {
+			fmt.Println("NRF return wrong status code", status)
+		}
+	}
+	return resouceNrfUri, retrieveNfInstanceID, err
+}
+
+func (s *nnrfService) SendDeregisterNFInstance() (problemDetails *models.ProblemDetails, err error) {
+	logger.ConsumerLog.Infof("Send Deregister NFInstance")
+
+	ctx, pd, err := pcf_context.GetSelf().GetTokenCtx(models.ServiceName_NNRF_NFM, models.NfType_NRF)
+	if err != nil {
+		return pd, err
+	}
+
+	pcfContext := s.consumer.pcf.Context()
+	// Set client and set url
+	client := s.getNFManagementClient(pcfContext.NrfUri)
+
+	var res *http.Response
+
+	res, err = client.NFInstanceIDDocumentApi.DeregisterNFInstance(ctx, pcfContext.NfId)
+	if err == nil {
+		return nil, nil
+	} else if res != nil {
+		defer func() {
+			if resCloseErr := res.Body.Close(); resCloseErr != nil {
+				logger.ConsumerLog.Errorf("DeregisterNFInstance response cannot close: %+v", resCloseErr)
+			}
+		}()
+		if res.Status != err.Error() {
+			return nil, err
+		}
+		problem := err.(openapi.GenericOpenAPIError).Model().(models.ProblemDetails)
+		problemDetails = &problem
+	} else {
+		err = openapi.ReportError("server no response")
+	}
+	return problemDetails, err
+}
---
+++ internal/sbi/consumer/udr_service.go
@@ -0,0 +1,135 @@
+package consumer
+
+import (
+	"strconv"
+	"strings"
+	"sync"
+
+	"github.com/free5gc/openapi"
+	"github.com/free5gc/openapi/models"
+	"github.com/free5gc/openapi/Nudr_DataRepository"
+	pcf_context "github.com/free5gc/pcf/internal/context"
+	"github.com/free5gc/pcf/internal/logger"
+	"github.com/free5gc/pcf/internal/util"
+)
+
+type nudrService struct {
+	consumer *Consumer
+
+	nfDataSubMu sync.RWMutex
+
+	nfDataSubClients map[string]*Nudr_DataRepository.APIClient
+}
+
+func (s *nudrService) getDataSubscription(uri string) *Nudr_DataRepository.APIClient {
+	if uri == "" {
+		return nil
+	}
+	s.nfDataSubMu.RLock()
+	client, ok := s.nfDataSubClients[uri]
+	if ok {
+		defer s.nfDataSubMu.RUnlock()
+		return client
+	}
+
+	configuration := Nudr_DataRepository.NewConfiguration()
+	configuration.SetBasePath(uri)
+	client = Nudr_DataRepository.NewAPIClient(configuration)
+
+	s.nfDataSubMu.RUnlock()
+	s.nfDataSubMu.Lock()
+	defer s.nfDataSubMu.Unlock()
+	s.nfDataSubClients[uri] = client
+	return client
+}
+
+func (s *nudrService) CreateInfluenceDataSubscription(ue *pcf_context.UeContext, request models.SmPolicyContextData) (
+	subscriptionID string, problemDetails *models.ProblemDetails, err error,
+) {
+	if ue.UdrUri == "" {
+		problemDetail := util.GetProblemDetail("Can't find corresponding UDR with UE", util.USER_UNKNOWN)
+		logger.ConsumerLog.Warnf("Can't find corresponding UDR with UE[%s]", ue.Supi)
+		return "", &problemDetail, nil
+	}
+	ctx, pd, err := pcf_context.GetSelf().GetTokenCtx(models.ServiceName_NUDR_DR, models.NfType_UDR)
+	if err != nil {
+		return "", pd, err
+	}
+	client := s.getDataSubscription(ue.UdrUri);
+	trafficInfluSub := s.buildTrafficInfluSub(request)
+	_, httpResp, localErr := client.InfluenceDataSubscriptionsCollectionApi.
+		ApplicationDataInfluenceDataSubsToNotifyPost(ctx, trafficInfluSub)
+	if localErr == nil {
+		locationHeader := httpResp.Header.Get("Location")
+		subscriptionID = locationHeader[strings.LastIndex(locationHeader, "/")+1:]
+		logger.ConsumerLog.Debugf("Influence Data Subscription ID: %s", subscriptionID)
+		return subscriptionID, nil, nil
+	} else if httpResp != nil {
+		defer func() {
+			if rspCloseErr := httpResp.Body.Close(); rspCloseErr != nil {
+				logger.ConsumerLog.Errorf("CreateInfluenceDataSubscription response body cannot close: %+v",
+					rspCloseErr)
+			}
+		}()
+		if httpResp.Status != localErr.Error() {
+			err = localErr
+			return subscriptionID, problemDetails, err
+		}
+		problem := localErr.(openapi.GenericOpenAPIError).Model().(models.ProblemDetails)
+		problemDetails = &problem
+	} else {
+		err = openapi.ReportError("server no response")
+	}
+	return "", problemDetails, err
+}
+
+func (s *nudrService) buildTrafficInfluSub(request models.SmPolicyContextData) models.TrafficInfluSub {
+	trafficInfluSub := models.TrafficInfluSub{
+		Dnns:             []string{request.Dnn},
+		Snssais:          []models.Snssai{*request.SliceInfo},
+		InternalGroupIds: request.InterGrpIds,
+		Supis:            []string{request.Supi},
+		NotificationUri: pcf_context.GetSelf().GetIPv4Uri() +
+			pcf_context.InfluenceDataUpdateNotifyUri + "/" +
+			request.Supi + "/" + strconv.Itoa(int(request.PduSessionId)),
+		// TODO: support expiry time and resend subscription when expired
+	}
+	return trafficInfluSub
+}
+
+func (s *nudrService) RemoveInfluenceDataSubscription(ue *pcf_context.UeContext, subscriptionID string) (
+	problemDetails *models.ProblemDetails, err error,
+) {
+	if ue.UdrUri == "" {
+		problemDetail := util.GetProblemDetail("Can't find corresponding UDR with UE", util.USER_UNKNOWN)
+		logger.ConsumerLog.Warnf("Can't find corresponding UDR with UE[%s]", ue.Supi)
+		return &problemDetail, nil
+	}
+	ctx, pd, err := pcf_context.GetSelf().GetTokenCtx(models.ServiceName_NUDR_DR, models.NfType_UDR)
+	if err != nil {
+		return pd, err
+	}
+	client := s.getDataSubscription(ue.UdrUri)
+	httpResp, localErr := client.IndividualInfluenceDataSubscriptionDocumentApi.
+		ApplicationDataInfluenceDataSubsToNotifySubscriptionIdDelete(ctx, subscriptionID)
+	if localErr == nil {
+		logger.ConsumerLog.Debugf("Nudr_DataRepository Remove Influence Data Subscription Status %s",
+			httpResp.Status)
+	} else if httpResp != nil {
+		defer func() {
+			if rspCloseErr := httpResp.Body.Close(); rspCloseErr != nil {
+				logger.ConsumerLog.Errorf("RemoveInfluenceDataSubscription response body cannot close: %+v",
+					rspCloseErr)
+			}
+		}()
+		if httpResp.Status != localErr.Error() {
+			err = localErr
+			return problemDetails, err
+		}
+		problem := localErr.(openapi.GenericOpenAPIError).Model().(models.ProblemDetails)
+		problemDetails = &problem
+	} else {
+		err = openapi.ReportError("server no response")
+	}
+	return problemDetails, err
+}
