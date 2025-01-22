014e692593d2154ccac04bf498bc83c73ac9ca8c
yzlin <linyangzhe0508@gmail.com>
2025-01-22 02:44:02 +0000

    feat: add charging data when updating traffic influence data

--- internal/sbi/processor/notifier.go
+++ internal/sbi/processor/notifier.go
@@ -9,6 +9,7 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 
 	"github.com/free5gc/openapi/models"
+	"github.com/free5gc/util/mongoapi"
 	"github.com/free5gc/pcf/internal/logger"
 	"github.com/free5gc/pcf/internal/util"
 	"github.com/free5gc/util/mongoapi"
